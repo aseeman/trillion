69fc09ceb8908b4c166507545dce8dc44601d02f
Ivan Malopinsky <ivan@imsky.co>
2015-12-05 22:45:51 -0500

    abstract match filter out

--- src/filter.js
+++ src/filter.js
@@ -1,12 +1,20 @@
+export function Match (haystack, needle) {
+  return haystack.indexOf(needle) !== -1;
+}
+
+const filters = {
+  'match': Match
+};
+
 export default function (type, field, value) {
   const name = `${type}-${field}-${value}`;
   let fn = function () {
     return true;
   };
 
-  if (type === 'match') {
+  if (filters[type]) {
     fn = function (data) {
-      return data[field].raw.indexOf(value) !== -1;
+      return filters[type](data[field].raw, value)
     };
   }
 
